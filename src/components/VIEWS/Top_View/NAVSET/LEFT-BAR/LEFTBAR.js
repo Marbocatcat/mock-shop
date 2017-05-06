ff644b5419c282a9c543957165385f7e0be564d1
Marbocatcat <mar.bocatcat@gmail.com>
2017-05-06 16:45:03 -0500

    Added Photo, and underline animation on LeftBar Nav

--- src/components/VIEWS/Top_View/NAVSET/LEFT-BAR/LEFTBAR.js
+++ src/components/VIEWS/Top_View/NAVSET/LEFT-BAR/LEFTBAR.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './LEFTBAR.css';
 
 const divStyle = {
-  paddingTop: ".5rem",
+  paddingTop: ".8rem",
 }
 
 const items = ["Shop Woman", "Shop Man", "Collections", "Blog", "Factory", "Brand Story", "Stores"].map((item, i) => (
-  <li style={ divStyle } key={ i }><a href="" >{ item }</a></li>
+  <li  style={ divStyle }  key={ i }><a href="">{ item }</a></li>
 ));
 
 
