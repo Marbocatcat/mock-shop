e65e18e0f5d63cd3b59db5c4a39c972092bdd541
Marbocatcat <mar.bocatcat@gmail.com>
2017-05-07 18:57:30 -0500

    Added grid template for Mid_View

--- src/components/VIEWS/Mid_View/MIDVIEW.js
+++ src/components/VIEWS/Mid_View/MIDVIEW.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './MIDVIEW.css';
 
+const images = ["./Assets/Image/photo1.jpg", "./Assets/Image/photo2.jpg", "./Assets/Image/photo1.jpg", "./Assets/Image/photo2.jpg","./Assets/Image/photo1.jpg","./Assets/Image/photo2.jpg"].map((photo, i) => (
+  <a href=""><li><img className="Image" src={ photo } alt=""/></li></a>
+));
+
 const MIDVIEW = (props) => {
   return (
     <section className="Mid_View">
-
+      <SlideBox />
     </section>
+  )
+};
+
+const SlideBox = (props) => {
+  return (
+    <div className="Card_Wrapper">
+      <ul>
+        { images }
+      </ul>
+    </div>
   )
 };
 
