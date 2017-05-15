24363e37bb39a31c3d6c81bfabcca3450aae7993
Marbocatcat <mar.bocatcat@gmail.com>
2017-05-15 18:27:36 -0500

    finished off footer, and adjusted padding on top view

--- src/components/FOOTER/FOOTER.js
+++ src/components/FOOTER/FOOTER.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import './FOOTER.css';
 
-function FOOTER () {
+
+const footerData = {
+  first: ["Products", "Services", "Press", "Newsletter"],
+  second: ["News", "LookBook", "Sustainablilty","About","Team"],
+  third: ["Shipping", "Terms & Conditions", "Privacy", "Imprint"]
+};
+
+function FOOTER (props) {
   return (
     <footer className="footer">
+      <ul className="footerList">
+        <Demands>
+          <h1>pinqponq</h1>
+        </Demands>
+        <Demands>
+          {footerData.first.map((list, key) =>
+            <li key={key}>{ list }</li>
+          )}
+        </Demands>
+
+        <Demands>
+          {footerData.second.map((list, key) =>
+            <li key={key}>{ list }</li>
+          )}
+        </Demands>
+
+        <Demands>
+          {footerData.third.map((list,key) =>
+            <li key={key}>{ list }</li>
+          )}
+        </Demands>
+      </ul>
     </footer>
+  )
+};
+
+const Demands = (props) => {
+  return (
+      <li>
+        <div>
+          <ul className="listList">
+            <a href="">
+              { props.children }
+            </a>
+          </ul>
+        </div>
+      </li>
   )
 };
 
